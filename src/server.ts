3270578d1caae22f88a97d81e617456d853c08ba
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-04-19 12:00:24 -0300

    Endpoint de busca

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,16 @@ import express from "express";
 
 const app = express();
 
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar uma informação específica
+ */
+
+app.get("/", (request, response) => {
+    return response.send("Olá Next Level Week 05")
+})
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
